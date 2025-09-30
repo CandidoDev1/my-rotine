4c7cd3a80c404a34e9203647f5517335dbb0d6af
CandidoDev1 <candido.ferreira@devtest.co.ao>
2025-09-30 17:42:43 +0100

    feat: update header component to Navbar with responsive design and routing, add react-router-dom dependencies

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import "./globals.css";
-import  { Header} from "@/components/header";
+import  Header  from "@/components/header";
 import { Metadata } from "next"
 
 export const metadata: Metadata = {
